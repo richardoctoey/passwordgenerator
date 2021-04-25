b14926240440d8b640f2ebaed7fba60e1426b713
richard <myrazel@gmail.com>
2021-04-26 01:29:13 +0700

    update

---
+++ go.mod
@@ -0,0 +1,8 @@
+module passwordgenerator
+
+go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.7.1 // indirect
+	github.com/sethvargo/go-password v0.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,17 +1,59 @@
 package main
 
 import (
-	  "log"
+    "strconv"
+    "github.com/sethvargo/go-password/password"
+    "github.com/gin-gonic/gin"
+)
 
-	    "github.com/sethvargo/go-password/password"
-    )
+func StringToInt(s string) int {
+    i, err := strconv.Atoi(s)
+    if err != nil {
+        return 0
+    }
+    return i
+}
 
-    func main() {
-	      // Generate a password that is 64 characters long with 10 digits, 10 symbols,
-	        // allowing upper and lower case letters, disallowing repeat characters.
-		  res, err := password.Generate(64, 10, 10, false, false)
-		    if err != nil {
-			        log.Fatal(err)
-				  }
-				    log.Printf(res)
-			    }
+func main() {
+    r := gin.Default()
+    r.GET("/ping", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "message": "pong",
+        })
+    })
+    r.GET("/password-generator", func(c *gin.Context){
+        length := StringToInt(c.Query("length"))
+        digitLength := StringToInt(c.Query("digit_length"))
+        digitSymbol := StringToInt(c.Query("digit_symbol"))
+        allowUpper := StringToInt(c.Query("allow_upper"))
+        allowUpperBool := false
+        disallowRepeat := StringToInt(c.Query("disallow_repeat"))
+        disallowRepeatBool := false
+
+        if allowUpper == 1 {
+            allowUpperBool = true
+        }
+
+        if disallowRepeat == 1 {
+            disallowRepeatBool = true
+        }
+
+
+
+        // Generate a password that is 64 characters long with 10 digits, 10 symbols,
+        // allowing upper and lower case letters, disallowing repeat characters.
+        res, err := password.Generate(length, digitLength, digitSymbol, allowUpperBool, disallowRepeatBool)
+        if err != nil {
+            c.JSON(200, gin.H{
+                "success": false,
+                "message": err.Error(),
+            })
+            return
+        }
+        c.JSON(200, gin.H{
+            "success": true,
+            "message": res,
+        })
+    })
+    r.Run()
+}
